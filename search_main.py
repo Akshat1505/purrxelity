2d80649c7b3fab9a81ceb75d5e6e2d156b3c91e8
Pranjwal-Jha <pranjwaljha2005@gmail.com>
2025-07-14 16:56:42 +0530

    Added Search Train Functionality

--- search_main.py
+++ search_main.py
@@ -1,11 +1,13 @@
 from langchain_core.messages import AIMessage, BaseMessage,HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.prebuilt import ToolNode
+from langchain_core.tools import tool
 from langchain_core.prompts import ChatPromptTemplate
 from langgraph.graph import add_messages,StateGraph
 from langgraph.constants import START,END
 from langchain_community.tools.tavily_search import TavilySearchResults
 from typing import Annotated,Sequence,List, TypedDict
+from train_status import search_train
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -19,7 +21,7 @@ class LLMNode():
     def __call__(self,state:BasicChat):
         last_message=state["messages"]
         prompt_template=ChatPromptTemplate.from_messages([
-            ("system","You are a helpful AI built to solve user queries with access to Search Feature if user asks for question that needs up to date information otherwise answer normally"),
+            ("system","You are a helpful AI built to solve user queries with access to Search Feature if user asks for question that needs up to date information otherwise answer normally. You can also use the search_train tool to find trains between two railway station to give user a consise answer for available train and coach classes available"),
             ("user","User input is {input}")
         ])
         filled_template=prompt_template.format(input=last_message)
@@ -27,10 +29,10 @@ class LLMNode():
             "messages":[self.llm.invoke(filled_template)]
         }
 
-model=ChatGoogleGenerativeAI(model="gemini-2.0-flash",temperature=0.7)
+model=ChatGoogleGenerativeAI(model="gemini-2.0-flash",temperature=1.0)
 search_tool=TavilySearchResults(max_result=5)
-tools=[search_tool]
-agent=LLMNode(llm=model.bind_tools([search_tool]))
+tools=[search_tool,search_train]
+agent=LLMNode(llm=model.bind_tools(tools))
 
 def ModelCallTool(state:BasicChat):
     last_message=state["messages"][-1]
--- train_status.py
+++ train_status.py
@@ -1,23 +1,31 @@
 import json,os,requests
 from datetime import date
-
-current_date=date.today()
-fi_date=str(current_date).replace("-","")
+from langchain_core.tools import tool
 
 
+@tool
 def search_train(source:str,destination:str):
+    """
+    Tool to get ticket availability in a train between source and destination station for current date
+    Args:
+    source(str): The source station code (e.g. "BE").
+    destination(str): The destination station code (e.g. "NDLS").
+    Returns:
+    A JSON object containing information about train availability, including train name, number and class availability with fares
+    abbreviation SL-Sleeper, 3A-Third AC, 2A-Second AC, 1A-First AC 
+    """
     departure_date=date.today().strftime("%Y%m%d")
     url = (
         "https://travel.paytm.com/api/trains/v5/search?"
         f"departureDate={departure_date}"
-        f"&destination={destination}"
+        f"&destination={destination.capitalize()}"
         f"&dimension114=direct-home"
         f"&isAscOfferEligible=false"
         f"&isH5=true"
         f"&is_new_user=false"
         f"&quota=GN"
         f"&show_empty=true"
-        f"&source={source}"
+        f"&source={source.capitalize()}"
         f"&user_type=active"
         f"&client=web"
         f"&deviceIdentifier=Mozilla%20Firefox-140.0"
@@ -29,14 +37,30 @@ def search_train(source:str,destination:str):
 
     response=requests.get(url,headers=headers)
 
-    print("Status -> ",response.status_code)
+    # print("Status -> ",response.status_code)
 
     if response.status_code==200:
-        return response.json()
+        train_data=response.json()["body"]["trains"]
+        if not train_data: return "No trains found for the given date"
+        formatted_result={}
+        for train in train_data:
+            curr_train=train["trainName"]+"-"+train["trainNumber"]
+            coach_class_stor_temp=[]
+            for coach_class in train["availability"]:
+                coach_class_stor_temp.append({
+                    "code":coach_class.get("code"),
+                    "fare":int(coach_class.get("fare")), #can explode use try except
+                    "status_shortform":coach_class.get("status_shortform")
+                })
+            formatted_result[curr_train]=coach_class_stor_temp
+        return formatted_result
+
     else:
         raise Exception(f"Failed with {response.status_code}")
-
-for train in search_train("BE","NDLS")["body"]["trains"]:
-    print(f"Train Name {train["trainName"]} Train Number {train["trainNumber"]}")
-    for coach_class in train["availability"]:
-        print(f"Class {coach_class["code"]} Fare {coach_class["fare"]} Aval {coach_class["status_shortform"]}") #fare not aval tatkal
+#
+# for train in search_train.invoke({"source":"BE","destination":"NDLS"})["body"]["trains"]:
+#     print(f"Train Name {train["trainName"]} Train Number {train["trainNumber"]}")
+#     for coach_class in train["availability"]:
+#         print(f"Class {coach_class["code"]} Fare {coach_class["fare"]} Aval {coach_class["status_shortform"]}") #fare not aval tatkal
+if __name__=="main":
+    print(search_train.invoke({"source":"BE","destination":"NDLS"}))
