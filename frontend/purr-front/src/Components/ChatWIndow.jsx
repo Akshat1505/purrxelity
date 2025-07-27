e1c0056ec6000250210dfba74268eb9771e1e0ff
akshat <gameratom15052005@gmail.com>
2025-07-27 23:56:15 +0530

    pages_comp

--- frontend/purr-front/src/Components/ChatWIndow.jsx
+++ frontend/purr-front/src/Components/ChatWIndow.jsx
@@ -3,14 +3,15 @@ import React from 'react'
 function ChatWIndow() {
   return (
     <div className='flex flex-col flex-1 p-6 justify-center items-center text-gray-300'>
-      <div className='text-5xl mb-4 font-bold'>purrxelity</div>
-      <div className='text-center text-gray-400 mb-4 text-2xl font-medium'>
-        Start Typing
+      <div className='relative flex justify-center items-center gap-4 mb-4 text-5xl font-bold'>
+        <span className='text-gray-400'>purrxelity</span>
+        <img src="logo1.svg" className='w-13 invert abosulte left-1/2-translate-x-[105%]' alt="" />
+        
       </div>
       <input
         type='text'
-        placeholder='Ask Anything..'
-        className='w-3/4 py-2 rounded-md text-white placeholder-gray-400 bg-[#404040] focus:outline-none'/>      
+        placeholder='Lets Go... Start Purring'
+        className='w-3/4 h-12 py-2 pl-4 rounded-md text-white placeholder-gray-400 border border-white/20 bg-[#404040] focus:outline-none mt-3'/>      
     </div>
   )
 }
--- frontend/purr-front/src/Components/Sidebar.jsx
+++ frontend/purr-front/src/Components/Sidebar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Sidebar() {
   return (
-    <div className='w-30 text-white h-screen bg-[#404040] p-4'>
-      <h2 className='text-xl font-semibold mb-4'>Sidebar</h2>
-      <ul className='space-y-2'>
+    <div className='w-30 text-white h-screen bg-[#090909] p-4 ml-6'>
+      <img src="logo1.svg" alt="" className='w-18 invert' />
+      <ul className='space-y-2 mt-10'>
         <li className='text-xs'>Home</li>
         <li className='text-xs'>Discover</li>
         <li className='text-xs'>Login/SignUp</li>
