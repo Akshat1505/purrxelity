1f454b74d126ecdd2a2485a5456512ddcb3374cb
akshat <gameratom15052005@gmail.com>
2025-07-28 22:40:24 +0530

    minor_ui

--- frontend/purr-front/src/Components/ChatWIndow.jsx
+++ frontend/purr-front/src/Components/ChatWIndow.jsx
@@ -4,11 +4,11 @@ function ChatWIndow() {
   return (
     <div className='flex flex-col flex-1 p-6 justify-center items-center text-gray-300'>
       <div className='relative flex justify-center items-center gap-4 mb-4 text-5xl font-bold'>
-        <span className='text-gray-400'>purrxelity</span>
+        <span style={{fontFamily:'GruvBox'}} className=' text-[#ebdbb2]'>purrxelity</span>
         <img src="logo1.svg" className='w-13 invert abosulte left-1/2-translate-x-[105%]' alt="" />
         
       </div>
-      <input
+      <input style={{fontFamily:'GruvBox'}}
         type='text'
         placeholder='Lets Go... Start Purring'
         className='w-3/4 h-12 py-2 pl-4 rounded-md text-white placeholder-gray-400 border border-white/20 bg-[#404040] focus:outline-none mt-3'/>      
--- frontend/purr-front/src/Components/Sidebar.jsx
+++ frontend/purr-front/src/Components/Sidebar.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Sidebar() {
+  const navigate = useNavigate();
+
   return (
-    <div className='w-30 text-white h-screen bg-[#090909] p-4 ml-6'>
+    <div style={{fontFamily:'GruvBox'}} className='w-30  text-white h-screen bg-[#665c54] p-4 flex flex-col justify-between '>
       <img src="logo1.svg" alt="" className='w-18 invert' />
-      <ul className='space-y-2 mt-10'>
-        <li className='text-xs'>Home</li>
-        <li className='text-xs'>Discover</li>
-        <li className='text-xs'>Login/SignUp</li>
+      <button className='mt-6 flex items-center space-x-2 text-white text-sm hover:text-gray-300'>
+        <div className='ml-4 mt-10 w-10 h-10 border border-white rounded-lg flex items-center justify-center'>
+          <img className=' w-10 invert cursor-pointer ' src="plus-svgrepo-com.svg" alt="" />
+        </div>
+      </button>
+      <ul style={{fontFamily:'GruvBox'}} className='space-y-2 mt-10'>
+        <li  className='text-lg '>History</li>
       </ul>
+      <button onClick={()=>navigate('/login')} className='flex-col cursor-pointer mb-4 flex items-center '>
+        <img className='mt-110 invert w-15 ' src="user.svg" alt="" />
+        <span className=' text-white text-lg'>Account</span>
+      </button>
     </div>
   )
 }
