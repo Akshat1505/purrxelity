d97f09f96aba5ffd934554335d0274348c273ac8
akshat <gameratom15052005@gmail.com>
2025-08-24 10:39:58 +0530

    implemented chatwindow sidebar regis

--- frontend/purr-front/src/Components/ChatWIndow.jsx
+++ frontend/purr-front/src/Components/ChatWIndow.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useRef } from 'react';
 import { useState } from 'react';
 import { ArrowRight } from 'lucide-react';
@@ -14,27 +14,52 @@ function ChatWIndow() {
   const [isChatStarted, setIsChatStarted] = useState(false);
   const [message,setMessage] = useState([]);
   const [isTyping , setIsTyping ] = useState(false);
+  const [threadId , setThreadId] = useState(null);
+  const [greet , setGreet] = useState('');
   const navigate = useNavigate();
 
+  useEffect(()=>{
+    const hour = new Date().getHours();
+    if(hour >= 5 && hour <12) setGreet('Good Morning');
+    else if(hour >= 12 && hour < 18) setGreet('Good Afternoon');
+    else setGreet('Good Evening');
+  },[])
+
+  const ensureThread = async () => {
+    if(threadId) return threadId;
+    const res = await fetch("http://localhost:8000/users/1/chats",{
+      method: "POST",
+      headers: {"Content-Type" : "application/json"},
+      body: JSON.stringify({thread_id: null,
+        messages: []
+      }),
+    });
+    const data = await res.json();
+    setThreadId(data.thread_id);
+    return data.thread_id;
+  }
   // handle ssending msg to backend
   const handleSend = async ()=>{
     if(!inputText.trim()) return;
 
     setMessage(prev=>[...prev, {sender:'user', text: inputText}])
     setInputText('');
-    const formData = new FormData();
-    formData.append('input', inputText);
+    const currentThread = await ensureThread();
 
     try{
-      const res = await fetch('http://localhost:8000/chat',{
+      const res = await fetch('http://localhost:8000/chat?user_id=1',{
         method: 'POST',
         headers: {'Content-Type' : 'application/json'},
-        body: JSON.stringify({input:inputText}),
-      })
+        body: JSON.stringify({input:inputText,
+          thread_id: currentThread,
+        }),
+      });
+      const data = await res.json();
+      console.log("reply" , data.message);
+      
       if(!res.ok){
         throw new Error(`Server responded with Status ${res.status}`)
       }
-      const data = await res.json();
       const typeText = (text)=>{
         let index = 0;  
         setMessage(prev=>[...prev,{sender:'ai' , text: ''}])
@@ -80,6 +105,11 @@ function ChatWIndow() {
 
   return (
     <div className='flex flex-col flex-1 p-6 justify-center items-center text-gray-300 rel'>
+      {greet && !isChatStarted && (
+        <div style={{fontFamily:'GruvBox'}} className='mb-2 text-4xl font-semibold text-gray-300'>
+          {greet}
+        </div>
+      )}
       <div className='relative flex justify-center items-center gap-4 mb-4 text-5xl font-bold'>
       </div>
       <div className='relative w-full h-full'>
@@ -87,43 +117,53 @@ function ChatWIndow() {
             Login/SignUp
         </button>
       </div>
-      <div className='flex flex-col w-full max-w-2xl mb-4 space-y-2'>
+      <div className='flex flex-col w-1/2 mx-atuo  space-y-2'>
         {message.map((msg,idx)=>(
-          <div key={idx} className={`flex ${msg.sender === 'user' ? 'justify-end' : 'justify-start'}`}>
+          <div key={idx} className={`flex ${msg.sender === 'user' ? 'justify-end ' : 'justify-start'}`}>
             <div  className={`px-4 py-2 rounded-md ${msg.sender === 'user' ? 'bg-[#212121] text-white' : 'bg-black text-gray-100'}`} style={msg.sender !== 'user' ? {fontFamily:'GruvBox'}:{}}>
               {msg.text}
             </div>
           </div>
         ))}
       </div>
+
       {/* Input Bar */}
-      <motion.div className=' flex w-1/2 mt-3' initial={{y: 0}} animate={isChatStarted ? {y: 50}: {y:0}} transition={{ease:'easeInOut', duration:0.5}}>
-        <div className='flex items-center bg-[#404040] rounded-md px-2 py-1 w-full' style={{fontFamily:'GruvBox'}}>
+
+      <motion.div className=' flex w-full justify-center mt-3' initial={{y: 0}} animate={isChatStarted ? {y: 50}: {y:0}} transition={{ease:'easeInOut', duration:0.5}}>
+        <div className='flex items-center bg-[#404040] rounded-md px-2 justify-center py-1 w-1/2' style={{fontFamily:'GruvBox'}}>
           <input
             type='text'
             placeholder='Lets Go... Start Purring'
             className='flex-1 h-12 py-2 pl-4 rounded-md text-white placeholder-gray-400 bg-transparent focus:outline-none'
             value={inputText}
             onChange={(e)=>setInputText(e.target.value)}
+            onKeyDown={(e)=>{
+              if(e.key === 'Enter'){
+                e.preventDefault();
+                handleSend();
+              }
+            }}
           />
           <button onClick={handleAttachmentClick} className='p-2 rounded hover:bg-[#505050] transition ml-2 cursor-pointer'>
             <img className='w-6 invert-75' src="attach.svg" alt="" />
           </button>
-          <button onClick={handleSend} className='p-2 rounded hover:bg-[#505050] transition ml-2 cursor-pointer flex items-center justify-center'>
+          <button onClick={handleSend} className='p-2 rounded mr-2 hover:bg-[#505050] transition ml-2 cursor-pointer flex items-center justify-center'>
             <ArrowRight className='invert' />
           </button>
+          <button onClick={()=>setActive(1)} className={`p-3 mr-2 cursor-pointer rounded-lg transition-all duration-200 ${active === 1 ? 'bg-black border border-white/50': ' opacity-60'}`}>
+          <img className='w-6 ml-1 invert-75' src="seach1.svg" alt="" />
+        </button>
+        <button onClick={()=>setActive(0)} className={`p-3 w-20 l cursor-pointer rounded-lg transition-all duration-200 ${active === 0 ? 'bg-black border border-white/50': ' opacity-60'}`}>
+          <img className='w-6 ml-3.5 invert-75' src="brain.svg" alt="" />
+        </button>
           {/* Hidden */}
           <input type="file" ref={fileInputRef} style={{display:'none'}} multiple onChange={hadnleFileChange} />
         </div>
       </motion.div>
-      {/* <div className='flex bg-[#1a1a1a] rounded-xl p-2 mr-130 scale-71  gap-2'>
-        <button onClick={()=>setActive(1)} className={`p-3 w-20l cursor-pointer rounded-lg transition-all duration-200 ${active === 1 ? 'bg-[#ebdbb2]/20 border border-[#ebdbb2]': ' opacity-60'}`}>
-          <img className='w-6 ml-1 invert-75' src="seach1.svg" alt="" />
-        </button>
-        <button onClick={()=>setActive(0)} className={`p-3 w-20 l cursor-pointer rounded-lg transition-all duration-200 ${active === 0 ? 'bg-[#ebdbb2]/20 border border-[#ebdbb2]': ' opacity-60'}`}>
-          <img className='w-6 ml-3.5 invert-75' src="brain.svg" alt="" />
-        </button>
-      </div> */}
+
+      {/* Toggle */}
+
+      
     </div>
   )
 }
--- frontend/purr-front/src/Components/Sidebar.jsx
+++ frontend/purr-front/src/Components/Sidebar.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios';
 
 function Sidebar() {
   const navigate = useNavigate();
+  const [chats, setChats] = useState([]);
+
+  useEffect(()=>{
+    const fetchChats = async ()=> {
+      try { 
+        const userId = 1;
+        const res = await axios.get(`http://localhost:8000/users/${userId}/chats`);
+        setChats(res.data);
+      } catch (error) {
+        console.error('Error fetching chats' , error); 
+      }
+    };
+    fetchChats();
+  }, []);
 
   return (
     <div style={{fontFamily:'GruvBox'}} className='group w-18 cursor-pointer hover:w-50 transition-all duration-250 text-white h-screen bg-[#282828] p-4 flex flex-col justify-between fixed top-0  '>
       <div className='flex justify-start group-hover:justify-center transition-all duration-250'>
         <img src="logo1.svg" alt="" className='w-18 invert' />
       </div>
-      <button className='flex  justify-start group-hover:justify-center items-center space-x-2 text-white text-sm hover:text-gray-300'>
+      <button onClick={()=> navigate('/chat/new')}  className='flex  justify-start group-hover:justify-center items-center space-x-2 text-white text-sm hover:text-gray-300'>
         <div className=' min-w-10 h-10 border border-white rounded-lg flex items-center justify-center'>
           <img className=' w-10 invert cursor-pointer ' src="plus-svgrepo-com.svg" alt="" />
         </div>
